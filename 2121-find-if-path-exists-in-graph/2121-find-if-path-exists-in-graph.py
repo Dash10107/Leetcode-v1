d81c4ec07889672f567a8d2156dccf7818b30acc
Daksh C Jain <97282628+Dash10107@users.noreply.github.com>
2025-06-17 18:02:47 +0530

    Time: 235 ms (96.71%), Space: 82 MB (99.08%) - LeetHub

---
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -0,0 +1,28 @@
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+
+    def find(self, i):
+        if self.parent[i] == i:
+            return i
+        self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x != root_y:
+            if self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            elif self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            else:
+                self.parent[root_y] = root_x
+                self.rank[root_x] += 1
+class Solution:
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        dsu = DSU(n)
+        for edge in edges:
+            dsu.union(edge[0],edge[1])
+        return dsu.find(source)==dsu.find(destination)
