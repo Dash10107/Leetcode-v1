f96f720b0285f8548b8421af8a391cee7c21879c
Dash <97282628+Dash10107@users.noreply.github.com>
2025-07-08 12:15:20 +0530

    Time: 0 ms (100%), Space: 17.8 MB (41.64%) - LeetHub

--- 3511-find-the-winning-player-in-coin-game/3511-find-the-winning-player-in-coin-game.py
+++ 3511-find-the-winning-player-in-coin-game/3511-find-the-winning-player-in-coin-game.py
@@ -1,8 +1,3 @@
 class Solution:
     def winningPlayer(self, x: int, y: int) -> str:
-        alice = False
-        while x>=1 and y>=4:
-            x-=1
-            y-=4
-            alice = not alice
-        return  "Alice" if alice else "Bob"
+        return "Alice" if min(x, y // 4) % 2 else "Bob"
