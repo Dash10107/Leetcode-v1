4a24eb6018459e93acce65051847a930f475f6a3
Dash <97282628+Dash10107@users.noreply.github.com>
2025-07-08 17:10:59 +0530

    Time: 6 ms (70.93%), Space: 17.9 MB (43.94%) - LeetHub

--- 3519-find-the-number-of-winning-players/3519-find-the-number-of-winning-players.py
+++ 3519-find-the-number-of-winning-players/3519-find-the-number-of-winning-players.py
@@ -1,11 +1,11 @@
 class Solution:
     def winningPlayerCount(self, n: int, pick: List[List[int]]) -> int:
-        d = defaultdict(list)
-        for x,y in pick:
-            d[x].append(y)
-        ans = 0
-        for i in d:
-            ch = max(Counter(d[i]).values())
-            if ch>=i+1:
-                ans+=1
-        return ans
+        res = 0
+        seen = set()
+        players = [[0] * 11 for _ in range(n)]
+        for x, y in pick:
+            if players[x][y] == x and x not in seen:
+                res += 1
+                seen.add(x)
+            players[x][y] += 1
+        return res
