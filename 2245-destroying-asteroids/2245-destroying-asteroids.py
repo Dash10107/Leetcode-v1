c4cd698ab71a5d651b8b547dad06525464e88a5e
Dash <97282628+Dash10107@users.noreply.github.com>
2025-07-17 12:53:20 +0530

    Time: 89 ms (23.22%), Space: 31.3 MB (73.93%) - LeetHub

---
+++ 2245-destroying-asteroids/2245-destroying-asteroids.py
@@ -0,0 +1,8 @@
+class Solution:
+    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+        asteroids.sort()
+        i = 0;n = len(asteroids)
+        while i<n and mass>=asteroids[i]:
+            mass+= asteroids[i]
+            i+=1
+        return i==n
