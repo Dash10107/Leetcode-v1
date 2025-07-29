80522ae834a46c6eb84eff3e354f75401dad0729
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2025-07-29 18:11:21 +0530

    Time: 0 ms (100%), Space: 19.1 MB (65.02%) - LeetHub

--- 0260-single-number-iii/0260-single-number-iii.py
+++ 0260-single-number-iii/0260-single-number-iii.py
@@ -1,8 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> List[int]:
-        c = Counter(nums)
-        ans = []
-        for ch in c:
-            if c[ch]==1:
-                ans.append(ch)
-        return ans
+        dist = 0
+        for n in nums:
+            dist ^=n
+        dist = (dist & -dist)
+        un1,un2 = 0,0
+        for n in nums:
+            if n & dist:
+                un1 ^= n
+            else:
+                un2 ^= n
+        return [un1,un2]
