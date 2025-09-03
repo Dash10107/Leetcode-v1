06979482c49abaee5e7319fdb8109ff915e7c933
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2025-09-03 23:36:38 +0530

    Time: 71 ms (44.83%), Space: 17.9 MB (26.44%) - LeetHub

--- 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.py
+++ 1418-fair-distribution-of-cookies/1418-fair-distribution-of-cookies.py
@@ -1,17 +1,21 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         n = len(cookies)
-        self.ans = float('inf')
-        sums = [0]*k
-        def dfs(i):
-            if i==n:
-                self.ans = min(self.ans,max(sums))
-                return
-            for j in range(k):
-                sums[j]+=cookies[i]
-                if max(sums)<self.ans:
-                    dfs(i+1)
-                sums[j]-=cookies[i]
-                if sums[j]==0:break
-        dfs(0)
-        return self.ans
+        mask = 1<<n
+        sums = [0]*mask
+        for m in range(mask):
+            s = 0
+            for i in range(n):
+                if m & (1<<i):
+                    s+=cookies[i]
+            sums[m]=s
+        dp = [[float('inf')]*(k+1) for _ in range(mask)]
+        dp[0][0]=0
+        for m in range(1,mask):
+            for g in range(1,k+1):
+                sub  = m
+                while sub:
+                    rem = m^sub
+                    dp[m][g] = min(dp[m][g],max(dp[rem][g-1],sums[sub]))
+                    sub = (sub-1)&m
+        return dp[mask-1][k]
