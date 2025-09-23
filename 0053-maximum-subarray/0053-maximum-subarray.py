a1b5bed0de4c708078488f7e7b91d45a972cc1d3
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2025-09-23 18:44:04 +0530

    Time: 12 ms (98.62%), Space: 32.1 MB (65.99%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,24 +1,8 @@
 class Solution:
     def maxSubArray(self, arr: List[int]) -> int:
-        def cross(l,mid,h):
-            left = float('-inf')
-            s = 0
-            for i in range(mid,l-1,-1):
-                s+=arr[i]
-                left = max(left,s)
-            s = 0
-            right = float('-inf')
-            for j in range(mid,h+1):
-                s+=arr[j]
-                right = max(right,s)
-            return max([left,right,left+right-arr[mid]])
-
-        def find(l,h):
-            if l>h:
-                return float('-inf')
-            elif l==h:
-                return arr[h]
-            else:
-                mid = (l+h)//2
-                return max([find(l,mid),find(mid+1,h),cross(l,mid,h)])
-        return find(0,len(arr)-1)
+        m = 0;ans=float('-inf')
+        for a in arr:
+            m+=a
+            if m>ans:ans=m
+            if m<0:m=0
+        return ans
