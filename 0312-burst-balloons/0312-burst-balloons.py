7ecd52b4ec901a0555aae34dd1b4d27b8d2f2963
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2025-10-02 11:21:52 +0530

    Time: 2653 ms (79.17%), Space: 19.7 MB (95.8%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -3,14 +3,11 @@ class Solution:
         nums = [1]+ nums + [1]
         n = len(nums)
         ans = 0
-        @cache
-        def func(left,right):
-            if right-left<=1:return 0
-            ans = 0
-            for i in range(left+1,right):
-                coins = nums[left]*nums[right]*nums[i]
-                l = func(left,i)
-                r = func(i,right)
-                ans = max(ans,coins+l+r)
-            return ans
-        return func(0,n-1)
+        dp = [[0]*n for _ in range(n)]
+        for le in range(2,n):
+            for left in range(n-le):
+                right= left+le
+                for i in range(left+1,right):
+                    coins = nums[left]*nums[right]*nums[i]
+                    dp[left][right] = max(dp[left][right],coins+dp[left][i]+dp[i][right])
+        return dp[0][n-1]
