4b5d4608ebb67f91d359160bdacf4f9f5b47a0d2
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2025-10-20 06:42:21 +0530

    Time: 10 ms (26.33%), Space: 20 MB (42.2%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -7,14 +7,9 @@
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
         s= set()
-        q = deque([root])
-        while q:
-            node = q.popleft()
-            if k-node.val in s:
-                return True
+        def dfs(node):
+            if node is None:return False
+            if k-node.val in s:return True
             s.add(node.val)
-            if node.left:
-                q.append(node.left)
-            if node.right:
-                q.append(node.right)
-        return False
+            return dfs(node.left) or dfs(node.right)
+        return dfs(root)
