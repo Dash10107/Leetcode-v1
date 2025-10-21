b2291c9fa8eb412372dcd37efa4d4243f5a437b8
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2025-10-21 06:41:18 +0530

    Time: 0 ms (100%), Space: 19.1 MB (84.41%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -1,16 +1,19 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if len(preorder)==0:
-            return None
-        r = preorder[0]
-        ind = inorder.index(r)
-        root = TreeNode(r)
-        root.left = self.buildTree(preorder[1:ind+1],inorder[:ind])
-        root.right = self.buildTree(preorder[ind+1:],inorder[ind+1:])
-        return root
+        idx_map = {val: i for i, val in enumerate(inorder)}
+        self.pre_idx = 0
+        def helper(left, right):
+            if left > right:
+                return None
+
+            # Pick current root from preorder
+            root_val = preorder[self.pre_idx]
+            self.pre_idx += 1
+            root = TreeNode(root_val)
+            mid = idx_map[root_val]
+            root.left = helper(left, mid - 1)
+            root.right = helper(mid + 1, right)
+
+            return root
+
+        return helper(0, len(inorder) - 1)
