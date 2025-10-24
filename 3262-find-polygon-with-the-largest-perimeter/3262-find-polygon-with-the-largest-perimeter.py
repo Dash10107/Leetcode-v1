d0fae3d6f88b4da6250f4972b860e8aacad56ed7
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2025-10-24 12:48:31 +0530

    Time: 147 ms (9.88%), Space: 31.1 MB (85.93%) - LeetHub

--- 3262-find-polygon-with-the-largest-perimeter/3262-find-polygon-with-the-largest-perimeter.py
+++ 3262-find-polygon-with-the-largest-perimeter/3262-find-polygon-with-the-largest-perimeter.py
@@ -2,10 +2,10 @@ class Solution:
     def largestPerimeter(self, nums: List[int]) -> int:
         n = len(nums)
         nums.sort()
-        pref =[0]*(n+1)
-        for i in range(n):pref[i+1]=nums[i]+pref[i]
-        ans = -1
-        for i in range(n-1,-1,-1):
-            if pref[i]>nums[i]:
-                ans = max(pref[i+1],ans)
+        pref =nums[0]+nums[1]
+        ans=-1
+        for i in range(2,n):
+            if pref>nums[i]:
+                ans=max(ans,pref+nums[i])
+            pref+=nums[i]
         return ans
