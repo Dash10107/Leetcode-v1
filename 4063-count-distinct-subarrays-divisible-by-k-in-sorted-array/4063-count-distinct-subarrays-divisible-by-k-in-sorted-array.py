ab68a2df0e96b30511303919b16ba7b3049b3f70
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2025-10-26 11:10:56 +0530

    Time: 292 ms (100%), Space: 43.7 MB (100%) - LeetHub

--- 4063-count-distinct-subarrays-divisible-by-k-in-sorted-array/4063-count-distinct-subarrays-divisible-by-k-in-sorted-array.py
+++ 4063-count-distinct-subarrays-divisible-by-k-in-sorted-array/4063-count-distinct-subarrays-divisible-by-k-in-sorted-array.py
@@ -1,32 +1,18 @@
-from math import lcm
-
 class Solution:
     def numGoodSubarrays(self, nums: List[int], k: int) -> int:
-        s = 0
-        dp = {0: 1}
-        ans = 0
-        cnts = 0
-        prev = None
 
-        for i in nums:
-            if i == prev:
-                cnts += 1
-            else:
-                cnts = 1
-                prev = i
-            
-            fact = lcm(k, i) // i
-            
-            subt = cnts // fact
-            if cnts % fact == 0:
-                subt -= 1
-            
-            s = (s + i) % k
-            
-            if s not in dp:
-                dp[s] = 0
-                
-            ans += dp[s] - subt
-            dp[s] += 1
-            
-        return ans
+        hmap=defaultdict(list)
+        hmap[0].append(-1)
+        total=0
+        count=0
+
+        for num in nums:
+            total=(total+num)%k
+            if total in hmap:
+                lst=hmap[total]
+                idx=bisect_left(lst,num)
+                count+=idx
+        
+            hmap[total].append(num)
+
+        return count
