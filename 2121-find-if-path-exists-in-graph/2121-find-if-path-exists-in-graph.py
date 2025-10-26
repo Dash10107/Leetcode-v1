9ef8ddbfdd4905bc4e3882d5c400bca6820b0c75
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2025-10-26 07:03:15 +0530

    Time: 8469 ms (5.01%), Space: 96.8 MB (60.84%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.py
@@ -1,28 +1,16 @@
-class DSU:
-    def __init__(self, n):
-        self.parent = list(range(n))
-        self.rank = [0] * n
-
-    def find(self, i):
-        if self.parent[i] == i:
-            return i
-        self.parent[i] = self.find(self.parent[i])
-        return self.parent[i]
-
-    def union(self, x, y):
-        root_x = self.find(x)
-        root_y = self.find(y)
-        if root_x != root_y:
-            if self.rank[root_x] < self.rank[root_y]:
-                self.parent[root_x] = root_y
-            elif self.rank[root_x] > self.rank[root_y]:
-                self.parent[root_y] = root_x
-            else:
-                self.parent[root_y] = root_x
-                self.rank[root_x] += 1
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        dsu = DSU(n)
-        for edge in edges:
-            dsu.union(edge[0],edge[1])
-        return dsu.find(source)==dsu.find(destination)
+        graph = defaultdict(list)
+        for u,v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+        q = deque([source])
+        vis=set()
+        while q:
+            node = q.popleft()
+            if node==destination:return True
+            vis.add(node)
+            for neg in graph[node]:
+                if neg not in vis:
+                    q.append(neg)
+        return False
