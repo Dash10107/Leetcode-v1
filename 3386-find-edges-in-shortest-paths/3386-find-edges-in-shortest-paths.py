8ac982432e8c49c509bd5e12b8db90abd9fd49bc
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2025-10-27 08:34:05 +0530

    Time: 511 ms (72.11%), Space: 54.7 MB (55.79%) - LeetHub

---
+++ 3386-find-edges-in-shortest-paths/3386-find-edges-in-shortest-paths.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findAnswer(self, n: int, edges: List[List[int]]) -> List[bool]:
+        graph = defaultdict(list)
+        for u,v,w in edges:
+            graph[u].append((v,w))
+            graph[v].append((u,w))
+        def djikstra(node):
+            heap = [(0,node)]
+            dist =[float('inf')]*n;dist[node]=0
+            while heap:
+                dis,curr=heappop(heap)
+                if dis > dist[curr]: 
+                    continue
+                for neg,w in graph[curr]:
+                    if dist[curr]+w<dist[neg]:
+                        dist[neg]=dist[curr]+w
+                        heappush(heap,(dist[neg],neg))
+            return dist
+        sdist = djikstra(0);ddist = djikstra(n-1)
+        ans = [False]*len(edges);short =sdist[n-1]
+        for i,e in enumerate(edges):
+            a,b,w=e
+            if short!=float('inf') and (sdist[a]+w+ddist[b]==short or sdist[b] + w + ddist[a] == short):
+                ans[i]=True
+        return ans
