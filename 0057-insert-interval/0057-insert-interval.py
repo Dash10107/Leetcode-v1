90e7a9a963fd436d310b1471a41bcc4a8a03c133
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2025-11-07 11:46:26 +0530

    Time: 2 ms (58.41%), Space: 20.7 MB (5.27%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.py
+++ 0057-insert-interval/0057-insert-interval.py
@@ -1,17 +1,16 @@
 class Solution:
     def insert(self, intervals: List[List[int]], new: List[int]) -> List[List[int]]:
-        ans = []
-        i,n = 0,len(intervals)
-        s,e=new
-        while i<n and intervals[i][1]<s:
-            ans.append(intervals[i])
-            i+=1
-        while i<n and intervals[i][0]<=e:
-            s = min(s,intervals[i][0])
-            e= max(e,intervals[i][1])
-            i+=1
-        ans.append([s,e])
-        while i<n:
-            ans.append(intervals[i])
-            i+=1
+        mp = defaultdict(int)
+        for s,e in intervals:
+            mp[s]+=1
+            mp[e]-=1
+        mp[new[0]]+=1
+        mp[new[1]]-=1
+        c=0;ans=[]
+        start=-1
+        for x  in sorted(mp.keys()):
+            if c==0:start=x
+            c+=mp[x]
+            if c==0:
+                ans.append([start,x])
         return ans
