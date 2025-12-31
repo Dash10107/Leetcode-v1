e68f6df8a96520057d5c9b089ee4da7cb2e827cc
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2025-12-31 09:29:40 +0530

    Time: 1240 ms (29.83%), Space: 37 MB (5.52%) - LeetHub

--- 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.py
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.py
@@ -6,19 +6,19 @@ class Solution:
             for i in range(mid):
                 grid[cells[i][0]-1][cells[i][1]-1]=1
             vis = set()
-            q = deque([])
-            for i in range(col):
-                if grid[0][i]==0:
-                    q.append((0,i))
-                    vis.add((0,i))
-            while q:
-                i,j = q.popleft()
+            def dfs(i,j):
+                if (i,j) in vis:return False
                 if i==row-1:return True
+                vis.add((i,j))
                 for dr,dc in dirr:
                     nr,nc = dr+i,dc+j
-                    if 0<=nr<row and 0<=nc<col and grid[nr][nc]==0 and (nr,nc) not in vis:
-                        q.append((nr,nc))
-                        vis.add((nr,nc))
+                    if 0<=nr<row and 0<=nc<col and grid[nr][nc]==0:
+                        if dfs(nr,nc):return True
+                return False
+            for i in range(col):
+                if grid[0][i]==0:
+                    if dfs(0,i):
+                        return True
             return False
         
         ans = 1
