500fdb8430661b81fb48b7d25c2f73fd3facab0b
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2026-01-11 07:53:02 +0530

    Time: 53 ms (23.32%), Space: 24.7 MB (5.85%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.py
+++ 0085-maximal-rectangle/0085-maximal-rectangle.py
@@ -1,20 +1,34 @@
 class Solution:
+    def largestRectangleArea(self, heights: List[int]) -> int:
+        n=len(heights);st = [-1]
+        left,right = [0]*n,[0]*n
+        ans = 0
+        for i in range(n):
+            id = st[-1]
+            while id!=-1  and heights[i]<= heights[id]:
+                st.pop()
+                id = st[-1]
+            st.append(i)
+            left[i]=id
+        st = [n]
+        for i in range(n-1,-1,-1):
+            id = st[-1]
+            while id!=n and heights[i]<=heights[id]:
+                st.pop()
+                id = st[-1]
+            st.append(i)
+            right[i]=id
+            ans = max(ans,heights[i]*(right[i]-left[i]-1))
+        return ans
     def maximalRectangle(self, matrix: List[List[str]]) -> int:
         n = len(matrix)
         m = len(matrix[0])
-        dp = [[(0,0)]*m for i in range(n)]
-        ans = 0
+        ans = 0;heights = [0]*m
+        for j in range(m):heights[j] = int(matrix[0][j])
         for i in range(n):
             for j in range(m):
-                if matrix[i][j]=='0':
-                    continue
-                left = top = 1
-                if i-1>=0:left += dp[i-1][j][1]
-                if j-1>=0:top += dp[i][j-1][0]
-                dp[i][j]=(top,left)
-                temp = top
-                for k in range(i,i-left,-1):
-                    temp = min(temp,dp[k][j][0])
-                    area = temp * (i-k+1)
-                    ans = max(area,ans)
-        return ans                
+                if i==0:continue
+                if matrix[i][j]=='0':heights[j]=0
+                heights[j]+= int(matrix[i][j])
+            ans = max(ans,self.largestRectangleArea(heights))
+        return ans
