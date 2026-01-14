ea66354be0d572cf9c4d4ca975c3daa1e21f130c
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2026-01-14 07:18:18 +0530

    Time: 0 ms (100%), Space: 19.8 MB (10.53%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -6,13 +6,8 @@ class Solution:
         for i in range(len(nums2)-2,-1,-1):
             while st and st[-1]<nums2[i]:
                 st.pop()
-            if not st:
-                st.append(nums2[i])
-                ans[nums2[i]]=-1
-            else:
-                ans[nums2[i]]=st[-1]   
-                st.append(nums2[i])
-            
+            ans[nums2[i]] = (st[-1] if st else -1)
+            st.append(nums2[i])
         res = []
         for n1 in nums1:
             res.append(ans[n1])
