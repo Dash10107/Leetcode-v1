c7b81127aa17c5b4a7fb3a0ff50fe8cf3ef6f011
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2026-01-15 09:03:53 +0530

    Time: 255 ms (85.62%), Space: 41.5 MB (58.22%) - LeetHub

---
+++ 2102-sequentially-ordinal-rank-tracker/2102-sequentially-ordinal-rank-tracker.py
@@ -0,0 +1,20 @@
+class SORTracker:
+
+    def __init__(self):
+        self.s=SortedList()
+        self.i=0
+
+    def add(self, name: str, score: int) -> None:
+        self.s.add((-score,name))
+
+    def get(self) -> str:
+        _,name = self.s[self.i]
+        self.i+=1
+        return name
+
+
+
+# Your SORTracker object will be instantiated and called as such:
+# obj = SORTracker()
+# obj.add(name,score)
+# param_2 = obj.get()
