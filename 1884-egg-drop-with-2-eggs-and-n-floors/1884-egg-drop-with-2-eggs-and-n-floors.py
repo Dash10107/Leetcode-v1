ced50b54a1098b39ecd0a7f07fee8c48679a9d1b
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2026-02-11 10:34:31 +0530

    Time: 0 ms (100%), Space: 19.4 MB (30.73%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.py
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.py
@@ -1,16 +1,3 @@
 class Solution:
     def twoEggDrop(self, n: int) -> int:
-        left = 1;right=n
-        def good(x):
-            floors = 0
-            while x>0:
-                floors+=x
-                x-=1
-            return floors>=n
-        while left<right:
-            mid = (left+right)//2
-            if good(mid):
-                right = mid
-            else:
-                left = mid+1
-        return left
+        return math.ceil((math.sqrt(1 + 8*n) - 1) / 2)
