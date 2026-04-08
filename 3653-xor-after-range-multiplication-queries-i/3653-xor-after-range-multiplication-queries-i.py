e58fde35dfd0025e0601c93137da9d9a62477724
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2026-04-08 09:29:35 +0530

    Time: 1819 ms (5.13%), Space: 19.9 MB (96.34%) - LeetHub

---
+++ 3653-xor-after-range-multiplication-queries-i/3653-xor-after-range-multiplication-queries-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def xorAfterQueries(self, nums: List[int], queries: List[List[int]]) -> int:
+        mod = 10**9+7
+        for l,r,k,v in queries:
+            for idx in range(l,r+1,k):
+                nums[idx]= (nums[idx]*v)%mod
+                idx+=1
+        ans = 0
+        for n in nums:
+            ans ^= n
+        return ans
