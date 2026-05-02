bc904a6ef58f2ed7b786a192dc0352fe0c0244d1
Daksh Jain <97282628+Dash10107@users.noreply.github.com>
2026-05-02 09:02:38 +0530

    Time: 11 ms (86.76%), Space: 19.2 MB (94.1%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.py
+++ 0788-rotated-digits/0788-rotated-digits.py
@@ -1,15 +1,22 @@
 class Solution:
     def rotatedDigits(self, n: int) -> int:
-        neut={'0','1','8'}
-        transform = {'2','5','6','9'}
-        invalid = {'3','4','7'}
-        def check(x):
-            trans=False
-            for ch in x:
-                if ch in invalid:return False
-                if ch in transform:trans=True
-            return trans
+        neut={0,1,8}
+        transform = {2,5,6,9}
+        invalid = {3,4,7}
+        dp =[0]*(n+1)
         ans=0
-        for i in range(1,n+1):
-            if check(str(i)):ans+=1
+        for i in range(n+1):
+            if i<10:
+                if i in neut:dp[i]=1
+                elif i in transform:
+                    dp[i]=2
+                    ans+=1
+            else:
+                a=dp[i//10]
+                b=dp[i%10]
+                if a==1 and b==1:dp[i]=1
+                elif a>=1 and b>=1:
+                    dp[i]=2;ans+=1
+                
         return ans
+                
